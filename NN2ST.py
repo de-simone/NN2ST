7b72bdc6a75653642c08fca0d5ecece68e3422cb
tdjacques <t.d.jacques@gmail.com>
2018-07-15 10:32:04 +0200

    Python 2 compatibility

--- NN2ST.py
+++ NN2ST.py
@@ -8,6 +8,7 @@
 #-----------------------------------------------------------------------------#
 
 from __future__ import print_function
+from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
